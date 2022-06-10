ca0b7569b502e1a4a290190b85c3be2a2d1a2995
Sophie-CR <104981706+Sophie-CR@users.noreply.github.com>
2022-06-10 12:33:02 +0100

    Refactored JS and changed stlying
    Combined multiple JS functions into one, improved consistency of the code, and ensured that the city name displayed was the data returned by the API and not the search input value. Changed the source code link colour and the border radius of the weather app. Moved Celsius and Fahrenheit links up.

--- src/index.js
+++ src/index.js
@@ -15,13 +15,6 @@ let days = [
   "Thursday",
   "Friday",
   "Saturday",
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
 ];
 let day = days[now.getDay()];
 let months = [
@@ -40,23 +33,16 @@ let months = [
 ];
 let month = months[now.getMonth()];
 
-let h5 = document.querySelector("h5");
-h5.innerHTML = `${hour}:${minute} ${day} ${now.getDate()} ${month} ${now.getFullYear()}`;
+let displayTime = document.querySelector("#current-time-date");
+displayTime.innerHTML = `${hour}:${minute} ${day} ${now.getDate()} ${month} ${now.getFullYear()}`;
 
 let apiKey = "10c6e46bee088157ebfe63ac8c22ea67";
 
-//Search Results
-function searchLocation(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  let replace = document.querySelector("#current-city");
-  replace.innerHTML = searchInput.value;
-}
-let replaceLocation = document.querySelector("#form");
-replaceLocation.addEventListener("submit", searchLocation);
-
 function getWeatherSearch(response) {
-  console.log(response.data);
+  let searchLocationName = document.querySelector("#current-city");
+  searchLocationName.innerHTML = response.data.name;
+  let searchLocationCountry = document.querySelector("#current-country");
+  searchLocationCountry.innerHTML = response.data.sys.country;
   let searchLocationTemp = document.querySelector("#temp");
   searchLocationTemp.innerHTML = Math.round(response.data.main.temp);
   let searchLocationDescription = document.querySelector("#description");
@@ -78,30 +64,14 @@ function searchWeather(form) {
   let apiWeatherSearchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
   axios.get(`${apiWeatherSearchUrl}&appid=${apiKey}`).then(getWeatherSearch);
 }
-let replaceTemp = document.querySelector("#form");
-replaceTemp.addEventListener("submit", searchWeather);
+let replaceWeather = document.querySelector("#form");
+replaceWeather.addEventListener("submit", searchWeather);
 
-// GeoLocation Results
-function userWeather(response) {
-  let userCity = response.data.name;
-  let displayCity = document.querySelector("#current-city");
-  displayCity.innerHTML = `${userCity}`;
-  let locationTemp = Math.round(response.data.main.temp);
-  document.querySelector("#temp").innerHTML = locationTemp;
-  let locationWind = Math.round(response.data.wind.speed);
-  document.querySelector("#current-wind").innerHTML = locationWind;
-  let locationHumidity = Math.round(response.data.main.humidity);
-  document.querySelector("#current-humidity").innerHTML = locationHumidity;
-  let locationTempHigh = Math.round(response.data.main.temp_max);
-  document.querySelector("#current-high").innerHTML = locationTempHigh;
-  let locationTempLow = Math.round(response.data.main.temp_min);
-  document.querySelector("#current-low").innerHTML = locationTempLow;
-}
 function useMyLocation(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiGeoWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiGeoWeatherUrl}&appid=${apiKey}`).then(userWeather);
+  axios.get(`${apiGeoWeatherUrl}&appid=${apiKey}`).then(getWeatherSearch);
 }
 function userLocation(event) {
   event.preventDefault();
@@ -126,19 +96,3 @@ function convertCelsius(event) {
 }
 let replaceCelsius = document.querySelector("#celsius");
 replaceCelsius.addEventListener("click", convertCelsius);
-
-// Forecast
-let dayOne = document.querySelector("#dayOne");
-dayOne.innerHTML = days[now.getDay() + 1];
-
-let dayTwo = document.querySelector("#dayTwo");
-dayTwo.innerHTML = days[now.getDay() + 2];
-
-let dayThree = document.querySelector("#dayThree");
-dayThree.innerHTML = days[now.getDay() + 3];
-
-let dayFour = document.querySelector("#dayFour");
-dayFour.innerHTML = days[now.getDay() + 4];
-
-let dayFive = document.querySelector("#dayFive");
-dayFive.innerHTML = days[now.getDay() + 5];
